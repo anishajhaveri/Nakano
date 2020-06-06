8cc899dd484df8e21c0a2b2e54e42a7d68018af7
anishajhaveri <54938773+anishajhaveri@users.noreply.github.com>
2020-06-07 04:08:21 +0530

    Added Text Alert system for the app.

--- app.py
+++ app.py
@@ -2,33 +2,46 @@
 import ibmiotf.application
 import time
 import json
+from twilio.rest import Client
+
 #Application Credentials
 options ={
 "org": "ds3c1j",
 "id": "app1",
 "auth-method": "apikey",
-"auth-key": "a-ds3c1j-wdmxph78xn",
+"auth-key": "",
 "auth-token": "" , #hidden for security purposes
 "clean-session": True
 }
+
+#Twilio Credentials for text alerts
+account_sid = ''
+auth_token = ''
+textclient = Client(account_sid, auth_token)
+
 #Subscribing to the event temperature for device type sensors :)
 sourceDeviceType="Sensors"
 sourceEvent = "temperature"
 
 
 def myEventCallback(event):
-    temp_threshold=99.5
-    str = "%s event received from device [%s]: %s"
-    print(str % (event.format, event.device, json.dumps(event.data)))
+    temp_threshold=99.9
+    str = "COVID Alert received from device [%s]: %s"
+    alert=str % (event.device, json.dumps(event.data))
+    #Alerts the user if the temperature is above a certain threshold
     if event.data["Temp"] > temp_threshold:
-        print("danger")
-    else:
-        print("fine")
+        message = textclient.messages.create(
+                                      from_='+12057368398',
+                                      body = alert,
+                                      to ='+919999999999'
+                                  )
 
-#connecting to IBM iot application client
+        print(message.sid)
+
+#Connecting to IBM iot application client
 client = ibmiotf.application.Client(options)
 client.connect()
 client.deviceEventCallback = myEventCallback
 client.subscribeToDeviceEvents(deviceType=sourceDeviceType,event=sourceEvent)
 while True:
-    time.sleep(1) #refreshes every 1second
+    time.sleep(1) #Every 1second
